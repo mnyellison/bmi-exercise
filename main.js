11957d015a184895b446517735dc783dfe41d833
mnyellison <nmatheus.moura@hotmail.com>
2026-04-24 12:25:43 -0300

    feat: validate user data and implement decision logic

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-const altura = parseFloat(prompt("Informe sua altura em metros (ex: 1.75):"));
-const peso = parseFloat(prompt("Informe seu peso em quilos (ex: 70):"));
+const alturaTexto = prompt("Informe sua altura em metros (ex: 1.75):");
+const pesoTexto = prompt("Informe seu peso em quilos (ex: 70):");
 
-console.log(altura);
-console.log(peso);
+if (alturaTexto !== null && alturaTexto !== "" && pesoTexto !== null && pesoTexto !== "") {
+    const altura = parseFloat(alturaTexto);
+    const peso = parseFloat(pesoTexto);
+    
+    if (!isNaN(altura) && !isNaN(peso)) {
+        console.log(altura);
+        console.log(peso);
+    } else {
+        alert("Ops! Valores inválidos. Por favor, verifique e tente novamente.");
+    }
+} else {
+    alert("Ops! Operação cancelada ou campos vazios.");
+}
