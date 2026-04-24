29afa9324a55dcd325ade47d6c9dc7456d214f53
mnyellison <nmatheus.moura@hotmail.com>
2026-04-23 23:30:17 -0300

    feat: start user data capture with prompt

---
+++ main.js
@@ -0,0 +1,5 @@
+const altura = parseFloat(prompt("Informe sua altura em metros (ex: 1.75):"));
+const peso = parseFloat(prompt("Informe seu peso em quilos (ex: 70):"));
+
+console.log(altura);
+console.log(peso);
