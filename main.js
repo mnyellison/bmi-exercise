fa240b427639951f3cd27f211fed2b8f04efeebb
mnyellison <nmatheus.moura@hotmail.com>
2026-04-28 22:55:00 -0300

    feat: add function for BMI classification

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// classificação da tabela
+function classificacaoIMC(imc) {
+    if (imc < 18.5) {
+        console.log(`${imc.toFixed(1)} - Abaixo do peso`);
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        console.log(`${imc.toFixed(1)} - Peso normal`);
+    } else if (imc >= 25.0 && imc <= 29.9) {
+        console.log(`${imc.toFixed(1)} - Sobrepeso`);
+    } else if (imc >= 30.0 && imc <= 34.9) {
+        console.log(`${imc.toFixed(1)} - Obesidade Grau I`);
+    } else {
+        console.log(`${imc.toFixed(1)} - Obesidade Severa/Mórbida`);
+    }
+}
+
 // entrada de dados
 const alturaTexto = prompt("Informe sua altura em metros (ex: 1.75):");
 const pesoTexto = prompt("Informe seu peso em quilos (ex: 70):");
@@ -16,20 +31,10 @@ if (alturaTexto !== null && alturaTexto !== "" && pesoTexto !== null && pesoText
         // verificando se os valores são maiores do que zero
         if ((altura > 0) && (peso > 0)) {
             console.log(`Altura: ${altura}`);
-            console.log(`Peso: ${peso}`);
+            console.log(`Peso: ${peso}`)
 
-            // classificação da tabela
-            if (imc < 18.5) {
-                console.log(`${imc.toFixed(1)} - Abaixo do peso`);
-            } else if (imc >= 18.5 && imc <= 24.9) {
-                console.log(`${imc.toFixed(1)} - Peso normal`);
-            } else if (imc >= 25.0 && imc <= 29.9) {
-                console.log(`${imc.toFixed(1)} - Sobrepeso`);
-            } else if (imc >= 30.0 && imc <= 34.9) {
-                console.log(`${imc.toFixed(1)} - Obesidade Grau I`);
-            } else {
-                console.log(`${imc.toFixed(1)} - Obesidade Severa/Mórbida`);
-            }
+            // passando as informações para a função
+            classificacaoIMC(imc);
         } else {
             alert("Os valores precisam ser maiores do que zero.");
         }
